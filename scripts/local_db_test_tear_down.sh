18f661623d9a9241f01935e6cd22e1829cdf6c3b
dmuruli <dmuruli@gandalfanalytics.com>
2024-07-16 17:42:09 -0700

    remove hardcoded passwords.

--- scripts/local_db_test_tear_down.sh
+++ scripts/local_db_test_tear_down.sh
@@ -1,11 +1,6 @@
 
-DB_SERVER_NAME=localhost
-DB_PORT=5432
-DB_ADMIN_NAME=service_app_admin
-DB_NAME=work_services
-DB_ADMIN_PASSWORD=adminPassw0rd1
-
-
+# parameters are set in the following script.
+source test_db_params.sh
 
 export PGPASSWORD=$DB_PASSWORD
 psql --host="${DB_SERVER_NAME}"  --port="${DB_PORT}" --dbname="${DB_NAME}"  --username="${DB_ADMIN_NAME}" - -f work_services_drop.sql
