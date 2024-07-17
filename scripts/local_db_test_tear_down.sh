88516be5ee8640cbc4e85c596a3094462475cb67
dmuruli <dmuruli@gandalfanalytics.com>
2024-07-16 17:43:29 -0700

    remove pwd

--- scripts/local_db_test_tear_down.sh
+++ scripts/local_db_test_tear_down.sh
@@ -2,5 +2,6 @@
 # parameters are set in the following script.
 source test_db_params.sh
 
+
 export PGPASSWORD=$DB_PASSWORD
 psql --host="${DB_SERVER_NAME}"  --port="${DB_PORT}" --dbname="${DB_NAME}"  --username="${DB_ADMIN_NAME}" - -f work_services_drop.sql
