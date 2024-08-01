885b11c74d1b67240ea1524f2b7f10a25211b101
dmuruli <dmuruli@gandalfanalytics.com>
2024-08-01 13:49:38 -0700

    Cleanup

--- src/api/schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class WorkOrder():
-    id: str
-    title: str
-    
-    
-    
--- src/model/workorder.py
+++
@@ -1,5 +0,0 @@
-from model.base import DBBaseModel
-
-
-class WorkOrder(DBBaseModel):
-    __tablename__ ='work_order'
